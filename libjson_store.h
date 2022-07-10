d98cb3b2f139a745dbc45b6069bd3ce705c59e66
joegasewicz <joegasewicz@gmail.com>
2022-07-10 13:27:53 +0100

    init:

---
+++ libjson_store.h
@@ -0,0 +1,6 @@
+#ifndef JSON_STORE_LIBJSON_STORE_H
+#define JSON_STORE_LIBJSON_STORE_H
+
+void json_store_new(void);
+
+#endif //JSON_STORE_LIBJSON_STORE_H
---
+++ libjson_store_test.c
@@ -0,0 +1,9 @@
+//
+// Created by Joe Geezer on 10/07/2022.
+//
+#include "libjson_store.h"
+
+int main(int argc, char** argv)
+{
+    json_store_new();
+}
